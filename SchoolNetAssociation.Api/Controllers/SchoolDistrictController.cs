84230b13cd78a90f5b5e0460ceabf214039b0db2
kumardeepak99 <dkumarjha999@gmail.com>
2023-07-28 21:08:41 +0530

    created a controller and added integration tests following tdd

---
+++ SchoolNetAssociation.Api/Controllers/SchoolDistrictController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolNetAssociation.Domain.Entities;
+
+namespace SchoolNetAssociation.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SchoolDistrictController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SchoolDistrict>>> GetAllSchoolDistrictsAsync()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}", Name = "GetSchoolDistrictById")]
+        public async Task<IActionResult> GetSchoolDistrictByIdAsync(string id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSchoolDistrictAsync(SchoolDistrict schoolDistrict)
+        {
+            if (schoolDistrict == null)
+            {
+                return BadRequest();
+            }
+            return CreatedAtRoute("GetSchoolDistrictById", new { id = schoolDistrict.Id }, schoolDistrict);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateSchoolDistrictAsync(string id, SchoolDistrict schoolDistrict)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSchoolDistrictAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SchoolNetAssociation.IntegrationTest/Controllers/SchoolDistrictControllerIntegrationTests.cs
@@ -0,0 +1,145 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Bogus;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using SchoolNetAssociation.Domain.Entities;
+
+namespace SchoolNetAssociation.IntegrationTest
+{
+    public class SchoolDistrictControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _httpClient;
+        private readonly Faker<SchoolDistrict> _schoolDistrictFaker;
+        private readonly Faker _faker;
+
+        const string ClientBaseUrl = "api/SchoolDistrict";
+
+        public SchoolDistrictControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _httpClient = factory.CreateClient();
+            _faker=new Faker();
+            _schoolDistrictFaker = new Faker<SchoolDistrict>()
+                .RuleFor(sd => sd.Name, f => f.Company.CompanyName())
+                .RuleFor(sd => sd.Description, f => f.Lorem.Sentence())
+                .RuleFor(sd => sd.City, f => f.Address.City())
+                .RuleFor(sd => sd.Superintendent, f => f.Name.FullName())
+                .RuleFor(sd => sd.IsPublic, f => f.Random.Bool())
+                .RuleFor(sd => sd.NumberOfSchools, f => f.Random.Int(1, 1000));
+        }
+
+        [Fact]
+        public async Task GetAllSchoolDistrictsAsync_ShouldReturnOkWithSchoolDistricts()
+        {
+            // Act
+            var response = await _httpClient.GetAsync(ClientBaseUrl);
+            var result = await response.Content.ReadFromJsonAsync<SchoolDistrict[]>();
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            result.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task GetSchoolDistrictByIdAsync_ExistingId_ShouldReturnOkWithSchoolDistrict()
+        {
+            // Arrange
+            var existingId = _faker.Random.Guid().ToString();
+            var expectedSchoolDistrict = _schoolDistrictFaker.Generate();
+            expectedSchoolDistrict.Id=existingId;
+            // Act
+            var response = await _httpClient.GetAsync($"{ClientBaseUrl}/{expectedSchoolDistrict.Id}");
+            var result = await response.Content.ReadFromJsonAsync<SchoolDistrict>();
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            result.Should().NotBeNull();
+            result.Should().Be(expectedSchoolDistrict);
+
+        }
+
+        [Fact]
+        public async Task GetSchoolDistrictByIdAsync_NonExistingId_ShouldReturnNotFound()
+        {
+            // Arrange
+            var nonExistingId = _faker.Random.Guid().ToString();
+
+            // Act
+            var response = await _httpClient.GetAsync($"{ClientBaseUrl}/{nonExistingId}");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task CreateSchoolDistrictAsync_ValidData_ShouldReturnCreatedAndCorrectData()
+        {
+            // Arrange
+            var newSchoolDistrict = _schoolDistrictFaker.Generate();
+
+            // Act
+            var response = await _httpClient.PostAsJsonAsync(ClientBaseUrl, newSchoolDistrict);
+            var createdSchoolDistrict = await response.Content.ReadFromJsonAsync<SchoolDistrict>();
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+            createdSchoolDistrict.Should().BeEquivalentTo(newSchoolDistrict, options => options.Excluding(x => x.Id));
+        }
+
+        [Fact]
+        public async Task UpdateSchoolDistrictAsync_ExistingIdAndValidData_ShouldReturnOkAndUpdatedData()
+        {
+            // Arrange
+            var existingId = _faker.Random.Guid().ToString();
+            var updatedSchoolDistrict = _schoolDistrictFaker.Generate();
+            updatedSchoolDistrict.Id=existingId;
+            // Act
+            var response = await _httpClient.PutAsJsonAsync($"{ClientBaseUrl}/{existingId}", updatedSchoolDistrict);
+            var updatedResult = await response.Content.ReadFromJsonAsync<SchoolDistrict>();
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            updatedResult.Should().Be(updatedSchoolDistrict);
+
+        }
+
+        [Fact]
+        public async Task UpdateSchoolDistrictAsync_NonExistingId_ShouldReturnNotFound()
+        {
+            // Arrange
+            var nonExistingId = _faker.Random.Guid().ToString();
+            var updatedSchoolDistrict = _schoolDistrictFaker.Generate();
+
+            // Act
+            var response = await _httpClient.PutAsJsonAsync($"{ClientBaseUrl}/{nonExistingId}", updatedSchoolDistrict);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task DeleteSchoolDistrictAsync_ExistingId_ShouldReturnNoContent()
+        {
+            // Arrange
+            var existingId = _faker.Random.Guid().ToString();
+
+            // Act
+            var response = await _httpClient.DeleteAsync($"{ClientBaseUrl}/{existingId}");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        }
+
+        [Fact]
+        public async Task DeleteSchoolDistrictAsync_NonExistingId_ShouldReturnNotFound()
+        {
+            // Arrange
+            var nonExistingId = _faker.Random.Guid().ToString();
+
+            // Act
+            var response = await _httpClient.DeleteAsync($"{ClientBaseUrl}/{nonExistingId}");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+    }
+}
